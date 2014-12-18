5061b089406636c06cdd171a620c97a09eddd12a
tas-group-6 <vincedalla@aol.de>
2014-12-18 12:26:15 +0100

    In allen vier ecken können nun pose estimates gesendet werden, sowohl im uhrzeigersinn also auch gegen den uhrzeigersinn

--- Martin/pose_estimation_tool/src/main.cpp
+++ Martin/pose_estimation_tool/src/main.cpp
@@ -11,29 +11,29 @@ geometry_msgs::Pose p[8];
 
 void fill_posearray()
 {
-  p[0].position.x  = 20.0;  p[0].position.y  = 10.0;  p[0].position.z  = 0.0;
-  p[0].orientation.x = 1.0; p[0].orientation.y = 1.0; p[0].orientation.z = 1.0; p[0].orientation.w = 0.0;
+  p[0].position.x  = 23.650;  p[0].position.y  = 18.892;  p[0].position.z  = 0.0;
+  p[0].orientation.x = 0.0; p[0].orientation.y = 0.0; p[0].orientation.z = -0.716; p[0].orientation.w = 0.698;
 
-  p[1].position.x  = 20.0;  p[1].position.y  = 10.0;  p[1].position.z  = 0.0;
-  p[1].orientation.x = 1.0; p[1].orientation.y = 1.0; p[1].orientation.z = 1.0; p[1].orientation.w = 0.0;
+  p[1].position.x  = 23.194;  p[1].position.y  = 5.878;  p[1].position.z  = 0.0;
+  p[1].orientation.x = 0.0; p[1].orientation.y = 0.0; p[1].orientation.z = 0.999; p[1].orientation.w = 0.042;
 
-  p[2].position.x  = 20.0;  p[2].position.y  = 10.0;  p[2].position.z  = 0.0;
-  p[2].orientation.x = 1.0; p[2].orientation.y = 1.0; p[2].orientation.z = 1.0; p[2].orientation.w = 0.0;
+  p[2].position.x  = 10.372;  p[2].position.y  = 6.707;  p[2].position.z  = 0.0;
+  p[2].orientation.x = 0.0; p[2].orientation.y = 0.0; p[2].orientation.z = 0.681; p[2].orientation.w = 0.732;
 
-  p[3].position.x  = 20.0;  p[3].position.y  = 10.0;  p[3].position.z  = 0.0;
-  p[3].orientation.x = 1.0; p[3].orientation.y = 1.0; p[3].orientation.z = 1.0; p[3].orientation.w = 0.0;
+  p[3].position.x  = 11.409;  p[3].position.y  = 19.745;  p[3].position.z  = 0.0;
+  p[3].orientation.x = 0.0; p[3].orientation.y = 0.0; p[3].orientation.z = -0.043; p[3].orientation.w = 0.999;
 
-  p[4].position.x  = 20.0;  p[4].position.y  = 10.0;  p[4].position.z  = 0.0;
-  p[4].orientation.x = 1.0; p[4].orientation.y = 1.0; p[4].orientation.z = 1.0; p[4].orientation.w = 0.0;
+  p[4].position.x  = 23.743;  p[4].position.y  = 19.084;  p[4].position.z  = 0.0;
+  p[4].orientation.x = 0.0; p[4].orientation.y = 0.0; p[4].orientation.z = 0.999; p[4].orientation.w = 0.034;
 
-  p[5].position.x  = 20.0;  p[5].position.y  = 10.0;  p[5].position.z  = 0.0;
-  p[5].orientation.x = 1.0; p[5].orientation.y = 1.0; p[5].orientation.z = 1.0; p[5].orientation.w = 0.0;
+  p[5].position.x  = 23.552;  p[5].position.y  = 5.924;  p[5].position.z  = 0.0;
+  p[5].orientation.x = 0.0; p[5].orientation.y = 0.0; p[5].orientation.z = 0.725; p[5].orientation.w = 0.689;
 
-  p[6].position.x  = 20.0;  p[6].position.y  = 10.0;  p[6].position.z  = 0.0;
-  p[6].orientation.x = 1.0; p[6].orientation.y = 1.0; p[6].orientation.z = 1.0; p[6].orientation.w = 0.0;
+  p[6].position.x  = 10.319;  p[6].position.y  = 6.554;  p[6].position.z  = 0.0;
+  p[6].orientation.x = 0.0; p[6].orientation.y = 0.0; p[6].orientation.z = -0.026; p[6].orientation.w = 1.0;
 
-  p[7].position.x  = 20.0;  p[7].position.y  = 10.0;  p[7].position.z  = 0.0;
-  p[7].orientation.x = 1.0; p[7].orientation.y = 1.0; p[7].orientation.z = 1.0; p[7].orientation.w = 0.0;
+  p[7].position.x  = 11.09;  p[7].position.y  = 19.695;  p[7].position.z  = 0.0;
+  p[7].orientation.x = 0.0; p[7].orientation.y = 0.0; p[7].orientation.z = -0.720; p[7].orientation.w = 0.694;
 }
 
 void draw_help()
@@ -52,10 +52,10 @@ int main(int argc, char **argv)
   fill_posearray();
   draw_help();
 
-  ros::init(argc, argv, "pose_esitmation_tool");
+  ros::init(argc, argv, "pose_estimation_tool");
   ros::NodeHandle n;
 
-  ros::Publisher pub = n.advertise<geometry_msgs::PoseWithCovarianceStamped>("pose_estimation", 1);
+  ros::Publisher pub = n.advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 1);
 
   geometry_msgs::PoseWithCovarianceStamped pose_estimation;
 
